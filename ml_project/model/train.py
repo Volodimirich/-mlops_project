fbef38eb749b7e92423111678e2c38e9fbfc3a4d
Volodimirich <shaposh-vova@rambler.ru>
2022-11-01 23:07:01 +0300

    First commit

---
+++ ml_project/model/train.py
@@ -0,0 +1,19 @@
+# from sklearn.svm import SVC
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.datasets import make_classification
+# from sklearn.model_selection import train_test_split
+# from sklearn.pipeline import Pipeline
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.linear_model import LinearRegression
+# from sklearn.naive_bayes import GaussianNB
+#
+# def train_model():
+#     KFold(n_splits=2, random_state=None, shuffle=False)
+
+
+
+
+
+
+
+if __name__ == '__main__':
